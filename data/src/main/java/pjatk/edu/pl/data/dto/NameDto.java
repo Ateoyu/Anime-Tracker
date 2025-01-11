a94bd25b1856d2dbb551c9374f5d1491bb3d5b80
Marcu <ateosu@proton.me>
2025-01-11 23:26:55 +0100

    Infinite Scroll works for browse page

--- data/src/main/java/pjatk/edu/pl/data/dto/NameDto.java
+++ data/src/main/java/pjatk/edu/pl/data/dto/NameDto.java
@@ -2,13 +2,6 @@ package pjatk.edu.pl.data.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-//public class NameDto {
-//    @JsonProperty("full")
-//    private String full;
-//    @JsonProperty("native")
-//    private String local;
-//}
-
 public record NameDto(
         String full,
         @JsonProperty("native")
